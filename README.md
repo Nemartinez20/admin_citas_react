8980c2c566a61fc046d131f0d483f8d539fc9de5
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2024-11-11 18:27:02 -0500

    se agrega funcionalidad de localstorage

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-##Proyecto de  crear citas para pacientes  con react
+## Proyecto de  crear citas para pacientes  con react
 Este es un proyecto para crear , eliminar y editar citas con react,
 ademas  implementando localstorage
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Form from "./components/Form";
 import Citas from "./components/Citas";
 
 function App() {
-  const [citasAll, setCitasAll] = useState([]);
+  //Citas en local Storage
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  const [citasAll, setCitasAll] = useState(citasIniciales);
+
+  //useEffect para realizar tareas cuando las citas cmbien o el arreglo de dependecias
+
+  useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citasAll));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citasAll]);
 
   //Crear citas
   const crearCita = (cita) => {
@@ -18,6 +35,12 @@ function App() {
     // console.log(citas);
   };
 
+  // titulo condicional
+  const titulo =
+    citasAll.length === 0
+      ? "No hay citas | Agrega una "
+      : "Administra tus citas";
+
   //Render a la vista
   return (
     <>
@@ -28,14 +51,10 @@ function App() {
             <Form crearCita={crearCita} />
           </div>
           <div className="container__citas item">
-            <h2 className="subtitle">Administra tus citas</h2>
-            {citasAll.length === 0 ? (
-              <h2 className="subtitle">No hay citas | Agrega una </h2>
-            ) : (
-              citasAll.map((cita) => (
-                <Citas key={cita.id} cita={cita} eliminarCita={eliminarCita} />
-              ))
-            )}
+            <h2 className="subtitle">{titulo}</h2>
+            {citasAll.map((cita) => (
+              <Citas key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
           </div>
         </section>
         <footer className="footer">
